e5295db7c01c1b14acce2cc58378b6375aaeca1f
John Liu <johnliu@windbornesystems.com>
2025-07-03 05:09:02 -0700

    esm module and commonjs support

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as Grapher } from './grapher.js';
+export { default as MultiGrapher } from './multigrapher.js';
+
+export { default } from './grapher.js'; 
---
+++ src/index.mjs
@@ -0,0 +1,5 @@
+export { default as Grapher } from './grapher.js';
+export { default as MultiGrapher } from './multigrapher.js';
+
+// Default export is the main Grapher component
+export { default } from './grapher.js'; 
