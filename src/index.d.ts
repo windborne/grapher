958ee89190d1f25076537c4115f28fd88fc99d4a
John Liu <johnliu@windbornesystems.com>
2025-07-18 20:41:57 -0700

    much stronger typscripting

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,37 +1,160 @@
 import React from 'react';
 
+export interface SeriesData {
+  data: any[] | { observe: (callback: Function) => void } | Function;
+  type?: 'values' | 'tuples' | 'objects' | 'tuple_observable' | 'object_observable' | 'infer';
+  xKey?: string;
+  yKey?: string;
+  xUnixDates?: boolean;
+  color?: string | number;
+  name?: string;
+  xLabel?: string;
+  yLabel?: string;
+  ignoreDiscontinuities?: boolean;
+  dashed?: boolean;
+  dashPattern?: number[];
+  width?: number;
+  rangeSelectorWidth?: number;
+  axis?: string | object;
+  expandYWith?: number[];
+  defaultAlwaysTooltipped?: boolean;
+  square?: boolean;
+  shiftXBy?: number;
+  graph?: number;
+  background?: object;
+  hideFromKey?: boolean;
+  showIndividualPoints?: boolean;
+  rendering?: 'line' | 'bar' | 'area';
+  negativeColor?: string;
+  gradient?: any[];
+  zeroLineWidth?: number;
+  zeroLineColor?: string;
+  zeroLineY?: number | string;
+  pointRadius?: number;
+  tooltipWidth?: number;
+  hasAreaBottom?: boolean;
+  shadowColor?: string;
+  rangeKey?: string;
+}
+
+export interface TooltipOptions {
+  includeSeriesLabel?: boolean;
+  includeXLabel?: boolean;
+  includeYLabel?: boolean;
+  includeXValue?: boolean;
+  includeYValue?: boolean;
+  floating?: boolean;
+  alwaysFixedPosition?: boolean;
+  floatPosition?: 'top' | 'bottom';
+  floatDelta?: number;
+  savingDisabled?: boolean;
+  customTooltip?: React.ComponentType<any>;
+  combineTooltips?: boolean | number;
+}
+
+export interface CustomBoundsSelector {
+  label: string;
+  calculator: (globalBounds?: any) => any;
+  datesOnly?: boolean;
+}
+
+export interface Annotation {
+  x?: string | number | Date;
+  startX?: string | number | Date;
+  endX?: string | number | Date;
+  series?: string[];
+  content?: string;
+  lineOnly?: boolean;
+}
+
+export interface DraggablePoint {
+  x: number;
+  y: number;
+  radius?: number;
+  fillColor?: string;
+  strokeColor?: string;
+  strokeWidth?: number;
+  onClick?: (point: DraggablePoint) => void;
+  onDoubleClick?: (point: DraggablePoint) => void;
+}
+
+export interface VerticalLine {
+  x: number;
+  color?: string;
+  lineTop?: number;
+  width?: number;
+  markTop?: boolean;
+  style?: object;
+  markerStyle?: object;
+  text?: string;
+  textTop?: number;
+  textStyle?: object;
+  minPixelX?: number;
+  maxPixelX?: number;
+  onRangeGraph?: boolean | object;
+  onRangeGraphOnly?: boolean;
+}
+
 export interface GrapherProps {
-  id: string;
-  series: any[];
-  theme?: string;
+  series: SeriesData[];
+
+  id?: string;
+  webgl?: boolean;
+  requireWASM?: boolean;
+  checkIntersection?: boolean;
+
+  onAxisChange?: (axes: any) => void;
+  onRenderTime?: (time: number) => void;
+  exportStateController?: (controller: any) => void;
+  onPointDrag?: (point: any) => void;
+  onDraggablePointsDoubleClick?: (point: any) => void;
+  onPointClick?: (point: any) => void;
+  timingFrameCount?: number;
+
+  stateControllerInitialization?: any;
+  syncPool?: any;
+  dragPositionYOffset?: number;
+
+  theme?: 'day' | 'night' | 'export';
+  title?: string;
+  fullscreen?: boolean;
+  bodyHeight?: number;
+  height?: number | string;
+  width?: number | string;
+
   showAxes?: boolean;
   showRangeGraph?: boolean;
   showRangeSelectors?: boolean;
   showSeriesKey?: boolean;
   showTooltips?: boolean;
+  showGrid?: boolean;
+  showAxisColors?: boolean;
+  bigLabels?: boolean;
+
+  xTickUnit?: 'year';
+  formatXAxisLabel?: (value: any) => string;
+  xAxisIntegersOnly?: boolean;
+  clockStyle?: '12h' | '24h';
+  timeZone?: string;
+  markRangeGraphDates?: boolean;
+
   boundsSelectionEnabled?: boolean;
-  customBoundsSelectors?: any[];
+  customBoundsSelectors?: CustomBoundsSelector[];
+  customBoundsSelectorsOnly?: boolean;
   sidebarEnabled?: boolean;
+  defaultBoundsCalculator?: string;
+
+  percentile?: number;
   defaultShowAnnotations?: boolean;
   defaultShowOptions?: boolean;
-  fullscreen?: boolean;
-  height?: number | string;
-  width?: number | string;
-  title?: string;
-  annotations?: any[];
-  onAxisChange?: (axes: any) => void;
-  onRenderTime?: (time: number) => void;
-  exportStateController?: (controller: any) => void;
-  stateControllerInitialization?: any;
-  timingFrameCount?: number;
-  percentile?: number;
-  showAxisColors?: boolean;
-  showGrid?: boolean;
-  bigLabels?: boolean;
+  defaultShowIndividualPoints?: boolean;
+  defaultShowSidebar?: boolean;
   defaultLineWidth?: number;
-  bodyHeight?: number;
-  dragPositionYOffset?: number;
-  syncPool?: any;
+
+  tooltipOptions?: TooltipOptions;
+  annotations?: Annotation[];
+  draggablePoints?: DraggablePoint[];
+  verticalLines?: VerticalLine[];
 }
 
 export interface MultiGrapherProps extends GrapherProps {
@@ -43,21 +166,23 @@ export interface MultiGrapherProps extends GrapherProps {
 
 export interface RangeSelectionProps {
   stateController: any;
-  customBoundsSelectors?: any[];
+  customBoundsSelectors?: CustomBoundsSelector[];
   customBoundsSelectorsOnly?: boolean;
   sidebarEnabled?: boolean;
 }
 
 export interface SyncPoolOptions {
   syncBounds?: boolean;
-  syncTooltips?: boolean;
+  syncTooltips?: boolean | 'onShift';
   syncDragState?: boolean;
 }
 
 declare const Grapher: React.ComponentType<GrapherProps>;
 declare const MultiGrapher: React.ComponentType<MultiGrapherProps>;
 declare const RangeSelection: React.ComponentType<RangeSelectionProps>;
-declare const SyncPool: any;
+declare const SyncPool: {
+  new (options?: SyncPoolOptions): any;
+};
 
 export const AVAILABLE_COLORS: string[];
 export const BUILT_IN_BOUND_CALCULATORS: Record<string, (globalBounds?: any) => any>;
