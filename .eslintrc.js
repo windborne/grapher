a13196cd8bad089943cd65d5dcb9af7a072df02c
John Liu <johnliu@windbornesystems.com>
2025-07-18 19:05:45 -0700

    bar autoscaling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,85 +1,80 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+      browser: true,
+      es6: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      project: './tsconfig.json', 
+    },
+    plugins: ['react', '@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
     ],
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "process": "readonly",
-
-        // for tests to not be sad
-        "require": "readonly",
-        "global": "readonly",
-        "expect": "readonly",
-        "describe": "readonly",
-        "it": "readonly"
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+      process: 'readonly',
+      require: 'readonly',
+      global: 'readonly',
+      expect: 'readonly',
+      describe: 'readonly',
+      it: 'readonly',
     },
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    rules: {
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'eol-last': 'error',
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 3,
+          maxEOF: 0,
+          maxBOF: 0,
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+      ],
+      'react/no-string-refs': 'off',
+      'no-useless-escape': 'off',
+      'react/no-unescaped-entities': [
+        'error',
+        {
+          forbid: ['>', '}'],
+        },
+      ],
+      'no-empty': 'off',
+      'comma-dangle': 'error',
+      'no-console': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'react/no-direct-mutation-state': 'error',
+      'react/no-unknown-property': 'error',
+      'no-undef': 'error',
+      'no-var': 'error',
+      'comma-spacing': 'error',
+      'func-call-spacing': ['error', 'never'],
+      'prefer-arrow-callback': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/prop-types': 'off',
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "eol-last": "error",
-        "no-multiple-empty-lines": [
-            "error", {
-                "max": 3,
-                "maxEOF": 0, // note: since we have eol-last, there actually will be 1 at the end of each file
-                "maxBOF": 0
-            }
-        ],
-        "react/no-string-refs": "off",
-        "no-useless-escape": "off",
-        "react/no-unescaped-entities": [
-            "error", {
-                "forbid": [">", "}"]
-            }
-        ],
-        "no-empty": "off",
-        "comma-dangle": "error",
-        "no-console": "error",
-
-        "no-unused-vars": [
-            "error", {
-                "varsIgnorePattern": "^_",
-                "argsIgnorePattern": "^_"
-            }
-        ],
-        "react/no-direct-mutation-state": "error",
-        "react/no-unknown-property": "error",
-        "no-undef": "error",
-        "no-var": "error",
-        "comma-spacing": "error",
-        "func-call-spacing": ["error", "never"],
-        "prefer-arrow-callback": "error"
-    },
-    "ignorePatterns": ["src/rust"]
-};
+    ignorePatterns: ['src/rust'],
+  };
+  
